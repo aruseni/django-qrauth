fca30d0e6f0825679d9b17f30c77898b40bff4c3
aruseni <aruseni.magiku@gmail.com>
2013-06-01 13:34:37 +0300

    Publish to PyPI

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='django-qrauth',
+    version='0.1',
+    author='aruseni',
+    author_email='aruseni.magiku@gmail.com',
+    packages=['qrauth'],
+    url='https://github.com/aruseni/django-qrauth',
+    license='BSD licence, see LICENCE.md',
+    description=('Nice QR codes that allow the users to instantly'
+                 ' sign in to the website on their mobile devices'),
+    long_description=open('README.md').read()[:-1],
+    zip_safe=False,
+    install_requires=['qrcode', 'redis'],
+)
