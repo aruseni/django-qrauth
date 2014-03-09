3e5eca97fadd7d72125598dc0c3693397382fe5b
Peter <dkdndes@users.noreply.github.com>
2014-03-09 23:15:25 +0100

    Replaced mimetype with content_type

--- qrauth/views.py
+++ qrauth/views.py
@@ -68,7 +68,7 @@ def qr_code_picture(request, auth_code):
     ])
 
     img = make_qr_code(login_link)
-    response = HttpResponse(mimetype="image/png")
+    response = HttpResponse(content_type="image/png")
     img.save(response, "PNG")
     return response
 
